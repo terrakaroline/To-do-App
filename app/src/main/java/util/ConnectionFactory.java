f8ca286f01c3c814b45c1cc168e06885d9ef8f03
Karoline <shiamosan@gmail.com>
2023-02-20 12:04:03 -0300

    first commit

---
+++ app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author karol
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+}
